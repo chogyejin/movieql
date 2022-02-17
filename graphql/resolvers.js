2a35256a200eba00f6898803163c46f1fd3fc328
chogyejin <whrpwls96@naver.com>
2022-02-17 23:57:38 +0900

    feat: install node-fetch, redefine schema and getMovies() function

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    // person: function () {
-    //   return chogyejin;
-    // },
     movies: () => getMovies(),
-    movie: (_, { id }) => getById(id), // {id} == agrs.id
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
